3d258425a7793676d2cf267b0d41971ad5498e5e
schickes <samuel.schicke@etu.univ-grenoble-alpes.fr>
2024-12-06 11:05:32 +0100

    Chargement initial du projet

---
+++ Portefolio/src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+function NavBar() {
+  return (
+    <div>
+      <ul>
+        <li>premier éléments</li>
+        <li>deuxieme éléments</li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
